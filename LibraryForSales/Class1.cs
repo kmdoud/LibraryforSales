9cc5b2de31177c8a1328b6195deea90dfc744ff7
Kenneth M Doud <kmdoud@gmail.com>
2019-02-26 11:11:06 -0500

    initial commit

--- LibraryForSales/Class1.cs
+++ LibraryForSales/Class1.cs
@@ -8,5 +8,24 @@ namespace LibraryForSales
 {
     public class Class1
     {
+        public Dictionary<string, int> Sales = new Dictionary<string, int>();
+
+        public int Get(string key)
+        {
+            return Sales[key];
+        }
+        public Class1()
+        {
+            Sales.Add("OH", 90416);
+            Sales.Add("KY", 77408);
+            Sales.Add("IN", 16588);
+            Sales.Add("PA", 55043);
+            Sales.Add("MI", 60099);
+            Sales.Add("NY", 19830);
+            Sales.Add("IL", 81154);
+            Sales.Add("WI", 11679);
+            Sales.Add("MN", 94207);
+            Sales.Add("MS", 93521);
+        }
     }
 }
---
+++ TestForSales/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestForSales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LibraryForSales.Class1 inst = new LibraryForSales.Class1();
+            var salestotal = inst.Get("IN") + inst.Get("NY") + inst.Get("MN");
+            Console.WriteLine($"Sales for IN, NY, MN = {salestotal}");
+
+            salestotal = inst.Get("KY") + inst.Get("PA") + inst.Get("MI") + inst.Get("IL");
+            Console.WriteLine($"Sales for KY, PA, MI, IL ={salestotal}");
+        }
+    }
+}
